b33223c7be8da11ad192a60aa6f6a9dd3e57510f
Rashadfrd <rashadnf@code.edu.az>
2022-10-18 23:24:11 +0400

    Login

--- Riode/Controllers/AccountController.cs
+++ Riode/Controllers/AccountController.cs
@@ -21,9 +21,27 @@ namespace Riode.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login(AppUser user)
+        public async Task<IActionResult> Login(UserLoginVM user)
         {
-            return View();
+            if(!ModelState.IsValid) return View();
+            var existUser = await _userManager.FindByEmailAsync(user.Email);
+            if (existUser is null)
+            {
+                ModelState.AddModelError("", "Email or Password is wrong!");
+                return View();
+            }
+            var result = await _signInManager.PasswordSignInAsync(existUser, user.Password, user.RememberMe,true);
+            if (!result.Succeeded)
+            {
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError("", "You made too many attemtps. Wait until -> " + existUser.LockoutEnd?.AddHours(4).DateTime.ToString("MM/dd/yyyy HH:mm:ss"));
+                    return View();
+                }
+                ModelState.AddModelError("", "Username or password is wrong");
+                return View();
+            }
+            return RedirectToAction("Index","Home");
         }
 
         public IActionResult Register()
@@ -39,6 +57,7 @@ namespace Riode.Controllers
             if (existUser != null)
             {
                 ModelState.AddModelError("Email", "This email has already been taken. Please,try too add new one");
+                return View();
             }
 
             AppUser user1 = new AppUser
@@ -46,7 +65,7 @@ namespace Riode.Controllers
                 Name = user.Name,
                 Surname = user.Surname,
                 Email = user.Email,
-                UserName = user.Name + "" + user.Surname,
+                UserName = user.Name
             };
             var result = await _userManager.CreateAsync(user1, user.Password);
 
@@ -60,6 +79,12 @@ namespace Riode.Controllers
             }
             await _signInManager.SignInAsync(user1,true);
             return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
         }
 
     }
---
+++ Riode/ViewModels/UserLoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Riode.ViewModels
+{
+    public class UserLoginVM
+    {
+        [Required,DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required,StringLength(40),DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
