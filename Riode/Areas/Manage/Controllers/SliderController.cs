b111cb2dcc9fb080b83741b27f21b2da2dd02d40
Rashadfrd <rashadnf@code.edu.az>
2022-10-21 02:32:13 +0400

    Slider crud ajax

--- Riode/Areas/Manage/Controllers/SliderController.cs
+++ Riode/Areas/Manage/Controllers/SliderController.cs
@@ -26,9 +26,8 @@ namespace Riode.Areas.Manage.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Slider? slider)
+        public IActionResult Create(Slider slider)
         {
-            if (slider is null) return BadRequest();
             if (slider.ImageFile is null) ModelState.AddModelError("ImageFile", "Please, choose image");
             if (!ModelState.IsValid) return View();
             var file = slider.ImageFile;
@@ -55,23 +54,68 @@ namespace Riode.Areas.Manage.Controllers
             if (id is null) return BadRequest();
             var slider = _context.Sliders.Find(id);
             if (slider is null) return NotFound();
-            string path = Path.Combine(_env.WebRootPath, "images/demos/demo1/slides/", slider.ImageUrl);
-            if (System.IO.File.Exists(path))
-            {
-                System.IO.File.Delete(path);
-            }
             if (slider.IsDeleted == false)
             {
                 slider.IsDeleted = true;
             }
             else
             {
-            _context.Sliders.Remove(slider);
+                string path = Path.Combine(_env.WebRootPath, "images/demos/demo1/slides/", slider.ImageUrl);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                _context.Sliders.Remove(slider);
             }
             _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Update(int? id)
+        {
+            if (id is null) return BadRequest();
+            var slider = _context.Sliders.Find(id);
+            if (slider is null) return NotFound();
+            return View(slider);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int? id, Slider slider)
+        {
+            if (id != slider.Id || id is null) return BadRequest();
+            if (slider.ImageFile is null) ModelState.AddModelError("ImageFile", "Please, choose image");
+            if(!ModelState.IsValid) return RedirectToAction(nameof(Update),new {id = id});
+            var editSlider = _context.Sliders.Find(id);
+            if (editSlider is null) return BadRequest();
+            var file = slider.ImageFile;
+            if (!file.CheckFileExtension("image/"))
+            {
+                ModelState.AddModelError("ImageFile", "File must be image");
+                return RedirectToAction(nameof(Update), new { id = id });
+            }
+            if (file.CheckFileSize(2))
+            {
+                ModelState.AddModelError("ImageFile", "File size can not be more than 2 mb!");
+                return RedirectToAction(nameof(Update), new { id = id });
+            }
+            string fileName = file.SaveImage(_env.WebRootPath, "images/demos/demo1/slides/");
+            string path = Path.Combine(_env.WebRootPath, "images/demos/demo1/slides/", editSlider.ImageUrl);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            slider.ImageUrl = fileName;
+            editSlider.ImageUrl = slider.ImageUrl;
+            editSlider.Order = slider.Order;
+            editSlider.Description = slider.Description;
+            editSlider.Title = slider.Title;
+            editSlider.SubTitle = slider.SubTitle;
+            editSlider.Place = slider.Place;
+            editSlider.IsModified = DateTime.UtcNow;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        } 
 
     }
 }
